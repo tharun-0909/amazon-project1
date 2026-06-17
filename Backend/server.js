275ecebedba222db1a005ce28a1cb142415655fa
tharun-0909 <tharungandla2006@gmail.com>
2026-06-17 17:58:05 +0530

    feat: configure render deployment blueprint, fix route require case sensitivity

--- Backend/server.js
+++ Backend/server.js
@@ -13,12 +13,14 @@ app.use(cors());
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/products", require("./routes/productRoutes"));
-app.use("/api/orders", require("./routes/orderRoutes"));
+app.use("/api/orders", require("./routes/orderroutes"));
 
 app.get("/", (req, res) => {
   res.send("API Running");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
