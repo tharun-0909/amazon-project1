9e6fa0fa639cefc61cee31c94b74b4459ca701f0
tharun-0909 <tharungandla2006@gmail.com>
2026-06-17 18:15:51 +0530

    refactor: rename orderroutes.js to orderRoutes.js and update imports

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/products", require("./routes/productRoutes"));
-app.use("/api/orders", require("./routes/orderroutes"));
+app.use("/api/orders", require("./routes/orderRoutes"));
 
 app.get("/", (req, res) => {
   res.send("API Running");
